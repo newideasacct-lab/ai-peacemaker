1862ebf07772a181eb246669158e2dd587f63fc8
newideasacct-lab <newideasacct-lab@github.com>
2026-06-26 01:08:58 -0700

    fix: add webhook handlers

---
+++ app/routes/webhooks.app.uninstalled.tsx
@@ -0,0 +1,6 @@
+﻿import type { ActionFunctionArgs } from "@remix-run/node";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  console.log("App uninstalled webhook received");
+  return new Response("OK", { status: 200 });
+};
